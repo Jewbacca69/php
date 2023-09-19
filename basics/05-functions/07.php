67da5512cd959a58e8f99f57e20430cc11b6520a
Jewbacca69 <141155995+Jewbacca69@users.noreply.github.com>
2023-09-19 18:35:47 +0300

    Create 07.php

---
+++ basics/05-functions/07.php
@@ -0,0 +1,53 @@
+<?php
+
+class Gun {
+    private string $name;
+    private int $price;
+    private string $license;
+
+    public function __construct($name, $price, $license) {
+        $this->name = $name;
+        $this->price = $price;
+        $this->license = $license;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function getPrice(): int {
+        return $this->price;
+    }
+
+    public function getLicense(): string {
+        return $this->license;
+    }
+}
+
+$weapons = [
+    new Gun('Gun1', 1200, 'a'),
+    new Gun('Gun2', 1100, 'a'),
+];
+
+$person = new stdClass();
+$person->name = 'John';
+$person->money = 8613;
+$person->licenses = ['c', 'd'];
+
+foreach($weapons as $weapon) {
+    $canBuy = canBuy($person, $weapon);
+    if ($canBuy) {
+        echo 'You can buy : ' . $weapon->getName() . PHP_EOL;
+    }
+}
+
+function canBuy(stdClass $person, Weapon $weapon): bool {
+    $personLicenses = $person->licenses;
+    $personMoney = $person->money;
+    $neededLicense = $weapon->getLicense();
+
+    $isPermitted = in_array($neededLicense, $personLicenses);
+    $hasMoney = $personMoney > $weapon->getPrice();
+
+    return $isPermitted && $hasMoney;
+}
