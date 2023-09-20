dae0bba85601a7e5377e27ab4cc5e3cc2f386248
Jewbacca69 <141155995+Jewbacca69@users.noreply.github.com>
2023-09-21 02:39:56 +0300

    Update 07.php

--- arithmetic-operations/07.php
+++ arithmetic-operations/07.php
@@ -4,4 +4,4 @@ $time = 10;
 
 $position = 0.5 * $gravity * $time * $time;
 
-echo "The position of the object after $time seconds is approximately: " . number_format($position, 2) . " meters\n";
+echo "The position of the object after $time seconds is : " . number_format($position, 2) . " meters\n";
