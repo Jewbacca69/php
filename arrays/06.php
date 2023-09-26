eb48e9dd015bb0f573e05b1142e35cf21b3c65e7
Jewbacca69 <141155995+Jewbacca69@users.noreply.github.com>
2023-09-26 16:28:00 +0300

    First commit

---
+++ arrays/06.php
@@ -0,0 +1,49 @@
+<?php
+
+$words = ["elephant", "giraffe", "kangaroo", "dolphin", "penguin"];
+
+$chosenWord = $words[array_rand($words)];
+
+$attempts = 6;
+$guessedLetters = [];
+
+echo "Welcome to Hangman!\n";
+
+while ($attempts > 0) {
+    $displayWord = "";
+    foreach (str_split($chosenWord) as $letter) {
+        if (in_array($letter, $guessedLetters)) {
+            $displayWord .= $letter;
+        } else {
+            $displayWord .= "_";
+        }
+    }
+
+    echo "Word: $displayWord\n";
+    echo "Misses: " . implode(", ", array_diff($guessedLetters, str_split($chosenWord))) . "\n";
+
+    if ($displayWord === $chosenWord) {
+        echo "Congratulations! You guessed the word: $chosenWord\n";
+        break;
+    }
+
+    $guess = strtolower(readline("Guess a letter: "));
+
+    if (in_array($guess, $guessedLetters)) {
+        echo "You already guessed '$guess'. Try again.\n";
+        continue;
+    }
+
+    $guessedLetters[] = $guess;
+
+    if (strpos($chosenWord, $guess) === false) {
+        $attempts--;
+
+        if ($attempts === 0) {
+            echo "Out of attempts! The word was: $chosenWord\n";
+            break;
+        }
+    }
+}
+
+echo "Thanks for playing!\n";
