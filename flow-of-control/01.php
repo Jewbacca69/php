142e6c5ad97d603f81c6f7704046c37eb8f8c502
Jewbacca69 <141155995+Jewbacca69@users.noreply.github.com>
2023-09-29 20:16:07 +0300

    First commit

---
+++ flow-of-control/01.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "Input the 1st number: ";
+$num1 = readline();
+
+echo "Input the 2nd number: ";
+$num2 = readline();
+
+echo "Input the 3rd number: ";
+$num3 = readline();
+
+echo max($num1, $num2, $num3);
